8f8e84187cb680e4c5e5f0f528ae4c99c21e6372
gsgaldino <gabrielsoaresii@outlook.com>
2026-04-16 19:51:29 -0300

    initial

---
+++ src/components/Hero/index.tsx
@@ -0,0 +1,13 @@
+export default function Hero () {
+  return (
+    <div>
+      <h4>✦ Coleções</h4>
+      <h2>Semijoias <span>TrêsMarias</span></h2>
+      <p>Peças criadas com amor e precisão. Cada detalhe é pensado para realçar sua beleza única e se tornar parte da sua memória afetiva.</p>
+      <div>
+        <button>Explorar coleção</button>
+        <button>Ver lookbook</button>
+      </div>
+    </div>
+  )
+}
